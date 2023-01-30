e9e8867406c63156dbe217a8a60ab4109e9729c7
jiayee9797@hotmail.com <jiayee9797@hotmail.com>
2023-01-31 00:49:17 +0800

    added android

---
+++ android/app/src/main/java/com/hadestech/phasercny/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hadestech.phasercny;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hadestech.phasercny',
+  appName: 'PhaserCNY',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
